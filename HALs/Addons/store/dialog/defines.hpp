834bb1f977c9179238d645b2ece0124c42a1689c
Hal Garland <halgarland@hotmail.co.uk>
2019-09-06 15:22:07 +0100

    Improved listbox appearance

--- HALs/Addons/store/dialog/defines.hpp
+++ HALs/Addons/store/dialog/defines.hpp
@@ -105,22 +105,79 @@
 
 
 class ScrollBar {
-	color[] = {1,1,1,0.6};
-	colorActive[] = {1,1,1,1};
-	colorDisabled[] = {1,1,1,0.3};
-	width = 0;
-	height = 0;
-	autoScrollEnabled = 0;
-	autoScrollSpeed = -1;
-	autoScrollDelay = 5;
-	autoScrollRewind = 0;
+	color[] = {1, 1, 1, 0.6};
+	colorActive[] = {1, 1, 1, 1};
+	colorDisabled[] = {1, 1, 1, 0.3};
 	thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
 	arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
 	arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
 	border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
 	shadow = 0;
 	scrollSpeed = 0.06;
+	width = 0;
+	height = 0;
+	autoScrollEnabled = 0;
+	autoScrollSpeed = -1;
+	autoScrollDelay = 5;
+	autoScrollRewind = 0;
 };
+
+class RscItemListBox {
+	deletable = 0;
+	fade = 0;
+	access = 0;
+	type = CT_LISTBOX;
+	rowHeight = "4.32 * (1 / (getResolution select 3)) * pixelGrid * 1.5"; //0; //"1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+	colorText[] = {1, 1, 1, 1};
+	colorDisabled[] = {1, 1, 1, 0.25};
+	colorScrollbar[] = {1, 0, 0, 0};
+	colorSelect[] = {0, 0, 0, 1};
+	colorSelect2[] = {0, 0, 0, 1};
+	colorSelectBackground[] = {0.95, 0.95, 0.95, 1};
+	colorSelectBackground2[] = {1, 1, 1, 0.5};
+	colorBackground[] = {1, 1, 1, 0.15};
+	soundSelect[] = {
+		"\A3\ui_f\data\sound\RscListbox\soundSelect",
+		0.09,
+		1
+	};
+	autoScrollSpeed = -1;
+	autoScrollDelay = 5;
+	autoScrollRewind = 0;
+	arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+	arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+	colorPicture[] = {1, 1, 1, 1};
+	colorPictureSelected[] = {1, 1, 1, 1};
+	colorPictureDisabled[] = {1, 1, 1, 0.25};
+	colorPictureRight[] = {1, 1, 1, 1};
+	colorPictureRightSelected[] = {1, 1, 1, 1};
+	colorPictureRightDisabled[] = {1, 1, 1, 0.25};
+	colorTextRight[] = {1, 1, 1, 1};
+	colorSelectRight[] = {0, 0, 0, 1};
+	colorSelect2Right[] = {0, 0, 0, 1};
+	tooltipColorText[] = {1, 1, 1, 1};
+	tooltipColorBox[] = {1, 1, 1, 1};
+	tooltipColorShade[] = {0, 0, 0, 0.65};
+
+	class ListScrollBar: ScrollBar {
+		color[] = {1, 1, 1, 1};
+		autoScrollEnabled = 1;
+	};
+
+	x = 0;
+	y = 0;
+	w = 0.3;
+	h = 0.3;
+	style = 16; //LB_TEXTURES
+	font = "RobotoCondensedLight";
+	shadow = 0;
+	colorShadow[] = {0, 0, 0, 0.5};
+	period = 0;
+	maxHistoryDelay = 1;
+	itemSpacing = 0; //"(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+	itemBackground[] = {0, 1, 1, 1};
+};
+
 
 class HALsStore_RscText {
 	deletable = 0;
@@ -146,71 +203,7 @@ class HALsStore_RscText {
 	tooltipColorText[] = {1,1,1,1};
 	tooltipColorBox[] = {0,0,0,0};
 };
-class HALsStore_RscListBox {
-	itemSpacing = 0.001;
-	itemBackground[] = {1, 1, 1, 0.1};
-	access = 0;
-	arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
-	arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
-	autoScrollDelay = 5;
-	autoScrollRewind = 0;
-	autoScrollSpeed = -1;
-	colorBackground[] = {1,1,1,0.2};
-	colorDisabled[] = {1,1,1,0.25};
-	colorPicture[] = {1,1,1,1};
-	colorPictureDisabled[] = {1,1,1,0.25};
-	colorPictureRight[] = {1,1,1,1};
-	colorPictureRightDisabled[] = {1,1,1,0.25};
-	colorPictureRightSelected[] = {1,1,1,1};
-	colorPictureSelected[] = {1,1,1,1};
-	colorScrollbar[] = {1,0,0,0};
-	colorSelect[] = {0,0,0,1};
-	colorSelect2[] = {0,0,0,1};
-	colorSelect2Right[] = {0,0,0,1};
-	colorSelectBackground[] = {0.95,0.95,0.95,1};
-	colorSelectBackground2[] = {1,1,1,0.5};
-	colorSelectRight[] = {0,0,0,1};
-	colorShadow[]={0,0,0,0.5};
-	colorText[] = {1,1,1,1};
-	colorTextRight[] = {1,1,1,1};
-	deletable = 0;
-	fade = 0;
-	font = "RobotoCondensed";
-	h = 0.3;
-	maxHistoryDelay = 1;
-	period = 1.2;
-	// rowHeight = 0; //rowHeight = "1.5 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)"; // 0
-	rowHeight = "2 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-	shadow = 0;
-	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	soundSelect[] = {"\A3\ui_f\data\sound\RscListbox\soundSelect",0.09,1};
-	style = 16;
-	tooltipColorBox[] = {1,1,1,1};
-	tooltipColorShade[] = {0,0,0,0.65};
-	tooltipColorText[] = {1,1,1,1};
-	type = 5;
-	w = 0.3;
-	x = 0;
-	y = 0;
 
-	class ListScrollBar {
-		arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
-		arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
-		autoScrollDelay = 5;
-		autoScrollEnabled = 1;
-		autoScrollRewind = 0;
-		autoScrollSpeed = -1;
-		border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
-		color[] = {1,1,1,1};
-		colorActive[] = {1,1,1,1};
-		colorDisabled[] = {1,1,1,0.3};
-		height = 0;
-		scrollSpeed = 0.06;
-		shadow = 0;
-		thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
-		width = 0;
-	};
-};
 class HALsStore_RscStructuredText {
 	type = 13;
 	style = 0;
